72ef45ec31ec8e86ea048c38c4a1ec6458b6d722
Tommy787576 <tommy787576@gmail.com>
2022-09-06 19:52:15 +0800

    Print a table finished

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable, int level) {
+  // there are 2^9 = 512 PTEs in a page table.
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      for (int i = 0; i < level; i++)
+        printf(" ..");
+      
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      vmprint((pagetable_t)child, level + 1);
+    } 
+    else if (pte & PTE_V) {
+      // leaf.
+      for (int i = 0; i < level; i++)
+        printf(" ..");
+
+      uint64 pa = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+    }
+  }
+}
