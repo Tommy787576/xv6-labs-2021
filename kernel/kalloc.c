e70c47108713563bbd634a0e90f2ac08d15660ad
Tommy787576 <tommy787576@gmail.com>
2024-09-29 15:53:42 +0800

    Finish q1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char name[10];  // the name of the lock
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(kmem[i].name, 10, "kmem_%d", i);
+    initlock(&kmem[i].lock, kmem[i].name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -46,6 +50,11 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  // cpuid() is only safe to call it and use its result
+  // when interrupts are turned off
+  push_off();
+  int cpu_id = cpuid();
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -55,11 +64,13 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  
+  acquire(&kmem[cpu_id].lock);
+  r->next = kmem[cpu_id].freelist;
+  kmem[cpu_id].freelist = r;
+  release(&kmem[cpu_id].lock);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +79,41 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  // cpuid() is only safe to call it and use its result
+  // when interrupts are turned off
+  push_off();
+  int cpu_id = cpuid();
+
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  acquire(&kmem[cpu_id].lock);
+  r = kmem[cpu_id].freelist;
+  if (!r) { 
+    // the one CPU must "steal" part of the other CPU's free list
+    // I steal all
+    int success = 0;
+    for (int i = 0; i < NCPU && !success; i++) {
+      if (i != cpu_id) {
+        acquire(&kmem[i].lock);
+        if (kmem[i].freelist) {
+          kmem[cpu_id].freelist = kmem[i].freelist;
+          kmem[i].freelist = 0; // freelist of core i is now empty
+          r = kmem[cpu_id].freelist;
+          // remember to break the loop once stealing is successful
+          success = 1;
+        }
+        release(&kmem[i].lock);
+      }
+    }
+  }
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu_id].freelist = r->next;
+  release(&kmem[cpu_id].lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  
+  pop_off();
+
   return (void*)r;
 }
